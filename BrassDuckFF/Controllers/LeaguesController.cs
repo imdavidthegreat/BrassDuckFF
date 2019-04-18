b3287620e5c3d4e0eacd67e3df8338994e1deb42
David Egley <davidegley88@gmail.com>
2019-04-18 00:18:38 -0700

    Rebuild after reconstruction of the database to include identity constraints for teamID, memberID, and leagueID

---
+++ BrassDuckFF/Controllers/LeaguesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BrassDuckFF.Models;
+
+namespace BrassDuckFF.Controllers
+{
+    public class LeaguesController : Controller
+    {
+        private BrassDuckEntities db = new BrassDuckEntities();
+
+        // GET: Leagues
+        public ActionResult Index()
+        {
+            var leagues = db.Leagues.Include(l => l.Member).Include(l => l.Team);
+            return View(leagues.ToList());
+        }
+
+        // GET: Leagues/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.Leagues.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // GET: Leagues/Create
+        public ActionResult Create()
+        {
+            ViewBag.leagueID = new SelectList(db.Members, "memberID", "username");
+            ViewBag.leagueID = new SelectList(db.Teams, "teamID", "teamname");
+            return View();
+        }
+
+        // POST: Leagues/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "leagueID,leaguename,commissID,passyrdsval,passtdsval,passintsval,rushyrdsval,rushtdsval,recrecval,recyrdsval,rectdsval,defsackval,defintsval,deftds,deffumrecval,kfgmadeval,kfgattemptval,kexpmadeval,kexpattemptval")] League league)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Leagues.Add(league);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.leagueID = new SelectList(db.Members, "memberID", "username", league.leagueID);
+            ViewBag.leagueID = new SelectList(db.Teams, "teamID", "teamname", league.leagueID);
+            return View(league);
+        }
+
+        // GET: Leagues/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.Leagues.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.leagueID = new SelectList(db.Members, "memberID", "username", league.leagueID);
+            ViewBag.leagueID = new SelectList(db.Teams, "teamID", "teamname", league.leagueID);
+            return View(league);
+        }
+
+        // POST: Leagues/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "leagueID,leaguename,commissID,passyrdsval,passtdsval,passintsval,rushyrdsval,rushtdsval,recrecval,recyrdsval,rectdsval,defsackval,defintsval,deftds,deffumrecval,kfgmadeval,kfgattemptval,kexpmadeval,kexpattemptval")] League league)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(league).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.leagueID = new SelectList(db.Members, "memberID", "username", league.leagueID);
+            ViewBag.leagueID = new SelectList(db.Teams, "teamID", "teamname", league.leagueID);
+            return View(league);
+        }
+
+        // GET: Leagues/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.Leagues.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // POST: Leagues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            League league = db.Leagues.Find(id);
+            db.Leagues.Remove(league);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BrassDuckFF/Controllers/TeamsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BrassDuckFF.Models;
+
+namespace BrassDuckFF.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private BrassDuckEntities db = new BrassDuckEntities();
+
+        // GET: Teams
+        public ActionResult Index()
+        {
+            var teams = db.Teams.Include(t => t.League).Include(t => t.Member);
+            return View(teams.ToList());
+        }
+
+        // GET: Teams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public ActionResult Create()
+        {
+            ViewBag.teamID = new SelectList(db.Leagues, "leagueID", "leaguename");
+            ViewBag.memberID = new SelectList(db.Members, "memberID", "username");
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "teamID,memberID,leagueID,teamname,qbID,rb1ID,rb2ID,wr1ID,wr2ID,teID,kID,defID")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Teams.Add(team);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.teamID = new SelectList(db.Leagues, "leagueID", "leaguename", team.teamID);
+            ViewBag.memberID = new SelectList(db.Members, "memberID", "username", team.memberID);
+            return View(team);
+        }
+
+        // GET: Teams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.teamID = new SelectList(db.Leagues, "leagueID", "leaguename", team.teamID);
+            ViewBag.memberID = new SelectList(db.Members, "memberID", "username", team.memberID);
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "teamID,memberID,leagueID,teamname,qbID,rb1ID,rb2ID,wr1ID,wr2ID,teID,kID,defID")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(team).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.teamID = new SelectList(db.Leagues, "leagueID", "leaguename", team.teamID);
+            ViewBag.memberID = new SelectList(db.Members, "memberID", "username", team.memberID);
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Team team = db.Teams.Find(id);
+            db.Teams.Remove(team);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
